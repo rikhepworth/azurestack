cdb02712603db6b0c6db243e911fd79982fec53e
Rik Hepworth <rik@blackmarble.com>
2019-02-17 13:25:57 +0000

    pointed ubuntu back at december image

--- deployment/offline/ConfigASDKdependencies.ps1
+++ deployment/offline/ConfigASDKdependencies.ps1
@@ -262,7 +262,8 @@ try {
     $row = $table.NewRow(); $row.Uri = "https://github.com/Azure/AzureStack-Tools/archive/master.zip"
     $row.filename = "Master.zip"; $row.path = "$ASDKpath"; $row.productName = "Azure Stack Tools"; $Table.Rows.Add($row)
     # Ubuntu Server 16.04 ZIP
-    $row = $table.NewRow(); $row.Uri = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.vhd.zip"
+        #$row = $table.NewRow(); $row.Uri = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.vhd.zip"
+    $row = $table.NewRow(); $row.Uri = "https://cloud-images.ubuntu.com/releases/xenial/release-20181207/ubuntu-16.04-server-cloudimg-amd64-disk1.vhd.zip"
     $row.filename = "UbuntuServer1.0.0.zip"; $row.path = "$ubuntuPath"; $row.productName = "Ubuntu Server 16.04 LTS zip file"; $Table.Rows.Add($row)
     # Ubuntu Server AZPKG
     $row = $table.NewRow(); $row.Uri = "https://github.com/rikhepworth/azurestack/raw/$branch/deployment/packages/Ubuntu/Canonical.UbuntuServer1604LTS-ARM.1.0.0.azpkg"
